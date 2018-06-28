dde9b224dcf287f7dfc37c8d90c67b18e766c6c5
buckethead1986 <rspeckpfs@gmail.com>
2018-06-28 12:50:42 -0400

    added dependent: :destroy to user model :poems and :favorited_poems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :poems
-  has_many :favorited_poems
+  has_many :poems, dependent: :destroy
+  has_many :favorited_poems, dependent: :destroy
   has_many :favorite_poems, through: :favorited_poems, source: :poem
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: "follower_id",
